030c6d6229c6f378cc25dd7565873cadf7e4dbc2
k01yasik <bzdykin@mail.ru>
2013-06-06 00:51:29 +0400

    ddd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flowers::Application.config.secret_token = '1d142bdc3981de0818d65c8770115b05d18cf944d88646e188edfcbc47462de13257e2667b842cdb9ccc062bc89c98420bb22946b12d72d891827f68b09f4415'
